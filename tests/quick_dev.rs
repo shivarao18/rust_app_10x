6778cec045968e27dce02eb8896f29cb585bbed3
shivarao18 <shiva.thummanapalli@gmail.com>
2025-08-28 08:12:12 -0400

    error improvement

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,22 @@
+#![allow(unused)] // For beginning only.
+
 use anyhow::Result;
+use serde_json::json;
 
-#[tokio::test]
-async fn quick_dev() -> Result<()> {
-    let hc = httpc_test::new_client("http://localhost:8080")?;
+#[tokio::main]
+async fn main() -> Result<()> {
+	let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello").await?.print().await?;
+	hc.do_get("/index.html").await?.print().await?;
 
-    Ok(())
+	let req_login = hc.do_post(
+		"/api/login",
+		json!({
+			"username": "demo1",
+			"pwd": "welcome"
+		}),
+	);
+	req_login.await?.print().await?;
+
+	Ok(())
 }
